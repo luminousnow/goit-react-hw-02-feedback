8f1824f7b38455fb2f07c46c3cb024fd9a0de5ae
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-02 15:32:05 +0200

    створює структуру, додає файли, налаштовує проект

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Newcomponents from './components/Newcomponents/Newcomponents';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Newcomponents title={'goit-react-hw-02-feedback'} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Newcomponents/Newcomponents.jsx
@@ -0,0 +1,8 @@
+import propTypes from 'prop-types';
+import './Newcomponents.module.css';
+
+function Newcomponents(params) {
+  return {};
+}
+
+export default Newcomponents;
