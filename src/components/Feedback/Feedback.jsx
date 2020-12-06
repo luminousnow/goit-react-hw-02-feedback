de3fb370a807d7496d523b152f068604ac9ac209
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-06 09:06:42 +0200

    додає подію onClick

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,17 +1,36 @@
+import React from 'react';
 import s from './Feedback.module.css';
 
-function Feedback(prpps) {
+function Feedback({ onGoodClick, onNeutralClick, onBadClick }) {
   return (
     <section className={s.section}>
       <h1 className={s.title}>Plese leave feedback</h1>
       <div className={s.buttonList}>
-        <button type="button" className={s.button}>
+        <button
+          type="button"
+          className={s.button}
+          data-action="good"
+          // onClick={onUpdate}
+          onClick={onGoodClick}
+        >
           Good
         </button>
-        <button type="button" className={s.button}>
+        <button
+          type="button"
+          className={s.button}
+          data-action="neutral"
+          // onClick={onUpdate}
+          onClick={onNeutralClick}
+        >
           Neutral
         </button>
-        <button type="button" className={s.button}>
+        <button
+          type="button"
+          className={s.button}
+          data-action="bad"
+          // onClick={onUpdate}
+          onClick={onBadClick}
+        >
           Bad
         </button>
       </div>
