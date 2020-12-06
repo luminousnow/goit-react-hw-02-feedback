c9a61b063fa2bc3f70f88a6cf47c6628ae446d1b
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-06 09:08:18 +0200

    рендерить статистику

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,18 +1,24 @@
 import s from './Statistics.module.css';
 
-function Statistics(props) {
+function Statistics({ statistics, totalFeedback, positiveFeedback }) {
   return (
     <section className={s.section}>
       <h1 className={s.title}>Statistics</h1>
       <ul className={s.statList}>
         <li className={s.stat}>
-          <span>Good:</span> {0}
+          <span>Good:</span> {statistics.good}
         </li>
         <li className={s.stat}>
-          <span>Neutral:</span> {0}
+          <span>Neutral:</span> {statistics.neutral}
         </li>
         <li className={s.stat}>
-          <span>Bad:</span> {0}
+          <span>Bad:</span> {statistics.bad}
+        </li>
+        <li className={s.stat}>
+          <span>Total:</span> {totalFeedback}
+        </li>
+        <li className={s.stat}>
+          <span>Positive feedback:</span> {positiveFeedback}
         </li>
       </ul>
     </section>
